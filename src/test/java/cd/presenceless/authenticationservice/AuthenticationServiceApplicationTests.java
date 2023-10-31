9644edc8cc017d6ac9c4e70cac99cb805489e5c6
lucienmakutano <lucien.programmer@gmail.com>
2023-10-31 17:14:13 +0300

    Init commit

---
+++ src/test/java/cd/presenceless/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cd.presenceless.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
