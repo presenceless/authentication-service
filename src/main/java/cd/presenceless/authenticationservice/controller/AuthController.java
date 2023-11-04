6cceb076b51931ad4034d2aef665ce82a0280db2
lucienmakutano <lucien.programmer@gmail.com>
2023-11-04 12:30:51 +0300

    method skeleton

--- src/main/java/cd/presenceless/authenticationservice/controller/AuthController.java
+++ src/main/java/cd/presenceless/authenticationservice/controller/AuthController.java
@@ -9,8 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
+    /**
+     * Authentication
+     * 3 forms
+     * organisation, attendants, admin(government officials)
+     * org jwtKeys are issued once per year
+     * attendants and gov jwtKeys are issued per auth request (last for 1 day)
+     * redirects are done through metadata
+     */
     @PostMapping
     public Object login(@RequestBody Object loginCred) {
+        return loginCred;
+    }
+
+    @PostMapping("/orgs")
+    public Object orgs(@RequestBody Object loginCred) {
+        return loginCred;
+    }
+
+    @PostMapping("/gov")
+    public Object gov(@RequestBody Object loginCred) {
         return loginCred;
     }
 }
